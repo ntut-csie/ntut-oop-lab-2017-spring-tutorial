70d54af08f53ed3d9b32a27a31a230918d945f19
A7FC\wupinyi <t104590020@ntut.org.tw>
2017-02-24 11:47:00 +0800

    add guess number

--- GitTutorial/GitTutorial.cpp
+++ GitTutorial/GitTutorial.cpp
@@ -2,6 +2,12 @@
 #include <iostream>
 using namespace std;
 bool Guess(int number) {
+	static int target = -1;
+	if (target == -1) {
+		target = (int)(100.0*rand()) % 100 + 1;
+	}
+	if (number == target)return true;
+	return false;
 }
 int main() {
 }
