a46e7b6098bf743d31a440538f15ed78f776ba85
Wei <weichun@csie.tw>
2017-02-24 11:49:39 +0800

    add meg

--- GitTutorial/GitTutorial.cpp
+++ GitTutorial/GitTutorial.cpp
@@ -3,7 +3,21 @@
 using namespace std;
 bool Guess(int number)
 {
-    return false;
+    static int target = -1;
+
+    if (number > target)
+    {
+        std::cout << "Smaller" << std::endl;
+        return false;
+    }
+    else if (number < target)
+    {
+        std::cout << "Bigger" << std::endl;
+        return false;
+    }
+
+    return true
+           return false;
 }
 int main()
 {
