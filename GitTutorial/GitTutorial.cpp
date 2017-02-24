72525e65fd5f842e4cd6852ef1ae67e3f3e74a03
Wei <weichun@csie.tw>
2017-02-24 11:42:35 +0800

    Implement main function

--- GitTutorial/GitTutorial.cpp
+++ GitTutorial/GitTutorial.cpp
@@ -1,7 +1,19 @@
 #include <string>
 #include <iostream>
 using namespace std;
-bool Guess(int number) {
+bool Guess(int number)
+{
+    return false;
 }
-int main() {
+int main()
+{
+    int guess;
+
+    do
+    {
+        std::cin >> guess;
+    }
+    while (!Guess(guess));
+
+    return 0;
 }
